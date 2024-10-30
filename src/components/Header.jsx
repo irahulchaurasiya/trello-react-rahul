7145448d7b999369d07fac5dff5fed3558cc43d8
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-10-30 12:30:06 +0530

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Box
+      width="100%"
+      textStyle="xl"
+      fontWeight="bold"
+      textAlign="center"
+      boxShadow="md"
+      p={"2"}
+    >
+      Trello
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
