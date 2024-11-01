b9c8c6a10231219311ceedadb43dcd34cbb4c2b4
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-11-02 01:03:46 +0530

    modify homepage path

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { Box, Button, Heading } from "@chakra-ui/react";
 import SingleBoardPage from "./pages/SingleBoardPage.jsx";
-
-const Home = () => {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-      height="100vh"
-    >
-      <Heading as="h4" size="lg" fontWeight="bold">
-        Click button to go to boards
-      </Heading>
-      <Button mt={4} colorScheme="blue" as={Link} to="/boards">
-        Click me
-      </Button>
-    </Box>
-  );
-};
-
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Homepage />} />
             <Route path="/boards" element={<Homepage />} />
             <Route path="/boards/:id" element={<SingleBoardPage />} />
             <Route path="*" element={<NotFoundPage />} />
