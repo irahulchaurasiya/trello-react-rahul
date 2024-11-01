976309722c20dad7d3736eba62266c8891c4b3dd
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-11-02 01:14:49 +0530

    delete boards file

--- src/components/Boards.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
-
-const Boards = ({ board }) => {
-  console.log(board);
-  return <Box>Board</Box>;
-};
-
-export default Boards;
