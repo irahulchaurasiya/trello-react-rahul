593922444fffd063d6ee97710e7d0a398ee41961
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-11-08 12:03:03 +0530

    remove unnecessary dependencies

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FaExclamationTriangle } from "react-icons/fa";
 import { Box, Button, Heading, Text } from "@chakra-ui/react";
