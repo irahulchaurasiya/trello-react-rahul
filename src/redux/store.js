71b5bde70dec9f67da1516f2ce2f7ec1e52bcd2a
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-11-11 13:03:58 +0530

    add checkitems and checklists reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import boardsReducer from '../redux/slices/boardsSlice';
 import singleBoardReducer from './slices/singleBoardSlice';
-import CardsSliceReducer from './slices/CardSlice';
+import CardsSliceReducer from './slices/cardSlice';
+import checkItemsReducer from './slices/checkItemsSlice';
+import checklistReducer from './slices/checklistSlice';
 
 export const store = configureStore({
   reducer: {
     boards : boardsReducer,
     singleBoard : singleBoardReducer,
     cards : CardsSliceReducer,
+    checkItems : checkItemsReducer,
+    checklists: checklistReducer,
   },
 })
